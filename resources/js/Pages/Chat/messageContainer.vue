66dc08529e626a4edc878e484f4c1ed10d2864da
InnocentWahome <innocentwahome@gmail.com>
2021-07-30 18:53:42 +0300

    Initial Commit

---
+++ resources/js/Pages/Chat/messageContainer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+      <div class="h-96 w-full">
+        <div class="h-full p-2 flex flex-col-reverse overflow-scroll">
+          <div v-for="(message, index) in messages" :key="index">
+            <message-item
+            :message="message"
+             />
+          </div>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import messageItem from './messageItem.vue'
+export default {
+  components: { messageItem },
+  props: ['messages']}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/Pages/Chat/messageItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+      {{message.user.name}}: {{message.message}}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['message']
+}
+</script>
+
+<style>
+
+</style>
